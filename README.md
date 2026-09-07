d28bc17234a892a27f8ce927c380acbd29bfaf69
Kad-19 <kidusasebe1919@gmail.com>
2026-09-07 13:10:13 +0300

    Initialize AWS CDK project with basic stack, configuration files, and necessary dependencies

---
+++ README.md
@@ -0,0 +1,3 @@
+# 01-basic-stack
+
+Run: npm install, npm run build, npx cdk synth, npx cdk diff. Deploy only when the course tells you. Destroy when done.
---
+++ lib/basic-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+export class BasicStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const bucket = new s3.Bucket(this, 'TrainingUploadsBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      versioned: false,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    cdk.Tags.of(bucket).add('Environment', 'Lab');
+  }
+}
